4524095efd45e468542c96abf4b8e33d4db41b31
rose-tetteh <rose.tetteh@amalitech.com>
2024-11-22 12:09:43 +0000

    feat:Add exception handler

--- src/main/java/com/example/textprocessingtool/DataManagement.java
+++ src/main/java/com/example/textprocessingtool/DataManagement.java
@@ -6,18 +6,26 @@ import java.util.regex.Pattern;
 public class DataManagement {
 
     public String searchText(String pattern, String text){
-        Pattern searchPattern = Pattern.compile(pattern);
-        Matcher matcher = searchPattern.matcher(text);
-        StringBuilder foundText = new StringBuilder();
-        while (matcher.find()) {
-            foundText.append(matcher.group()).append("\n");
+        try {
+            Pattern searchPattern = Pattern.compile(pattern);
+            Matcher matcher = searchPattern.matcher(text);
+            StringBuilder foundText = new StringBuilder();
+            while (matcher.find()) {
+                foundText.append(matcher.group()).append("\n");
+            }
+            return foundText.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
         }
-        return foundText.toString();
     }
 
     public String replaceText(String pattern, String text, String replacement){
-        Pattern searchPattern = Pattern.compile(pattern);
-        Matcher matcher = searchPattern.matcher(text);
-        return matcher.replaceAll(replacement);
+        try {
+            Pattern searchPattern = Pattern.compile(pattern);
+            Matcher matcher = searchPattern.matcher(text);
+            return matcher.replaceAll(replacement);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
